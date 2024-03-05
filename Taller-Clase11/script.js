5080be1d8bd0c79b7630a0b457d2e423be6286da
IsabellaCabrera <158528449+IsabellaCabrera@users.noreply.github.com>
2024-03-05 08:27:22 -0500

    Add files via upload

---
+++ Taller-Clase11/script.js
@@ -0,0 +1,7 @@
+const nombre = prompt ("Cual es su nombre");
+const apellido = prompt ("Cual es su apellido");
+const año = prompt ( "Cual es su año de nacimiento");
+const calcularedad= 2024 - parseInt( año);
+const dia = prompt ( " Cual es su dia de nacimiento?");
+const mes= prompt ( " Cual es su mes de nacimiento?");
+console.log ( " mi nombre es ", nombre ," mi apellido es ", apellido ,"mi edad es ", calcularedad , "y naci el ", dia, "de ", mes );
